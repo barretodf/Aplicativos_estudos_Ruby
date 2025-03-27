90346f9943473b5941fc8b7fef1ef36fc9b1cb31
Edmilson Barreto <edsb1981@gmail.com>
2025-03-27 01:10:30 -0300

    Teste do conexão com issue #4

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
+  #Adicionado para teste da issue #4
     validates :name, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true }
     validates :phone, length: { minimum: 8, maximum: 15, allow_blank: true }
