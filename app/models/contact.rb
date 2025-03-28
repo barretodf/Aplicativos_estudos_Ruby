2c1a43e6ce9a893713c54498f0703febd8c0a4c5
Edmilson Barreto <edsb1981@gmail.com>
2025-03-28 00:34:04 -0300

    Tópico 6: Melhora função de editar contatos e traduz mensagem de erro de e-mail para português

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   #Adicionado para teste da issue #4
     validates :name, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true, message: "não é válido" }
     validates :phone, length: { minimum: 8, maximum: 15, allow_blank: true }
   end
